ff6e8717e821f0e65669e06b7d2d4d3a6bff2da2
Leo Singer <leo.singer@ligo.org>
2013-04-12 18:33:46 -0700

    Set just the type object fields we need

--- bayestar/sky_map.c
+++ bayestar/sky_map.c
@@ -44,27 +44,11 @@ static void premalloced_dealloc(premalloced_object *self)
 
 static PyTypeObject premalloced_type = {
     PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "premalloced",             /*tp_name*/
-    sizeof(premalloced_object),/*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)premalloced_dealloc, /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-    "Pre-malloc'd memory",     /* tp_doc */
+    .tp_name = "premalloced",
+    .tp_basicsize = sizeof(premalloced_object),
+    .tp_dealloc = (destructor)premalloced_dealloc,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_doc = "Pre-malloc'd memory"
 };
 
 
