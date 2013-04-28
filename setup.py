167f12d38720ec7cb98b7bce4480f70289ad5271
Leo Singer <leo.singer@ligo.org>
2013-04-27 21:15:39 -0500

    Change license to GPLv2+

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #
 # Copyright (C) 2013  Leo Singer
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
 
@@ -28,7 +29,7 @@ setup(
     author='Leo Singer',
     author_email='leo.singer@ligo.org',
     description='Rapid Bayesian sky maps for gravitational wave inspirals',
-    license='GNU General Public License Version 3',
+    license='GNU GPLv2+',
     requires=['bayestar', 'healpy', 'numpy', 'glue', 'pylal', 'lal', 'lalsimulation'],
     namespace_packages=['bayestar'],
     packages=['bayestar'],
